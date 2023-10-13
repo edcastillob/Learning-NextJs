da93efd1db31f98c07dd9ceb5b99ce0b93577a6d
edcastillob <edwar.castillo@gmail.com>
2023-10-13 17:54:30 -0400

    Layout

--- 01-initial-demo/pages/about.js
+++ 01-initial-demo/pages/about.js
@@ -1,24 +1,30 @@
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import { MainLayout } from "@/components/Layouts/MainLayout";
+import { DarkLayout } from "@/components/Layouts/DarkLayout";
 
 const About = () => {
   return (
     <>
-      <MainLayout>
-        <h1 className={styles.center}>About</h1>
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-      </MainLayout>
+      <h1 className={styles.center}>About</h1>
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
     </>
+  );
+};
+About.getLayout = function getLayout(page) {
+  return (
+    <MainLayout>
+      <DarkLayout>{page}</DarkLayout>
+    </MainLayout>
   );
 };
 
