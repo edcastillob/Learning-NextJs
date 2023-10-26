43f135e7de8e5cadc8df327e4fd2e4bb2c1d2eca
edcastillob <edwar.castillo@gmail.com>
2023-10-26 16:12:29 -0400

    Navbar App-Next End projects

--- 04-app-nextjs/components/navbar/Navbar.jsx
+++ 04-app-nextjs/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="bg-slate-400 items-center px-20 font-bold text-black">
         <Link href="/">
         Home
         </Link>
