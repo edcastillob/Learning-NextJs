8ef8b56e1b8090b94f7a4c1078a373db5fb51e83
edcastillob <edwar.castillo@gmail.com>
2023-10-28 12:35:41 -0400

    Backend + interface

--- 05-nextjs-prisma/src/app/page.jsx
+++ 05-nextjs-prisma/src/app/page.jsx
@@ -1,9 +1,26 @@
+import TaskCard from "@/components/taskCard/TaskCard";
+import { prisma } from "@/libs/prisma";
 
 
-const HomePage = () => {
+const HomePage = async () => {
+  const tasks = await getTaskAll();
   return (
-    <div>Task</div>
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3 mt-10">
+    {
+      tasks.map((task) => (
+        <TaskCard task ={task} key={task.id}/>
+      ))
+    }
+  </div>
+    </section>
   )
-}
+};
 
 export default HomePage;
+
+async function getTaskAll() {
+  // const res = await fetch("http://localhost:3000/api/task");
+  // return await res.json();
+  return await prisma.task.findMany();
+}
